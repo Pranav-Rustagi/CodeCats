e96e1fa791c9d575773481ab1d0377b14116beea
Pranav-Rustagi <pranav001100@gmail.com>
2021-11-27 23:18:09 +0530

    10th problem solved

--- WEEKLY-TASKS/WEEK - 5/10-arrange-strings.c
+++ WEEKLY-TASKS/WEEK - 5/10-arrange-strings.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 short stringComp(char *str1, char *str2) {
     if(*str1 == '\0' && *str2 == '\0')
@@ -16,37 +16,45 @@ short stringComp(char *str1, char *str2) {
 }
 
 
-void swapStr(char **str1, char **str2) {
-    char *temp = *str1;
-    *str1 = *str2;
-    *str2 = temp;
+void swapRow(char arr[][100], int ind1, int ind2) {
+    int length1 = strlen(arr[ind1]), length2 = strlen(arr[ind2]);
+    int limit = length1 > length2 ? length1 : length2;
+    for(int i = 0 ; i < limit ; i++) {
+        char temp = arr[ind1][i];
+        arr[ind1][i] = arr[ind2][i];
+        arr[ind2][i] = temp;
+    }
+    arr[ind1][length2] = '\0';
+    arr[ind2][length1] = '\0';
 }
 
 
-void arrangeInLexo(char **arr, int n) {
+
+void arrangeInLexo(char arr[][100], int n) {
     for(int i = 0 ; i < n - 1 ; i++)
         for(int j = i + 1 ; j < n ; j++)
-            if(stringComp(arr + i, arr + j) == -1)
-                swapStr((arr + i), (arr + j));
+            if(stringComp(arr[i], arr[j]) == -1)
+                swapRow(arr, i, j);
 }
 
 
 int main() {
-    char **str;
     int strCount;
     printf("\n\nEnter no of inputs : ");
     scanf("%d", &strCount);
-    str = (char **) malloc(strCount * sizeof(char *)) ;
+    
+    char str[strCount][100];
+    printf("\nEnter strings one by one :\n\n");
     for(int i = 0 ; i < strCount ; i++) {
-        str[i] = (char *) malloc(100 * sizeof(char));
         printf("%2d. ", i + 1);
-        scanf("%s", str + i);
+        scanf("%s", *(str + i));
     }
 
     arrangeInLexo(str, strCount);
 
-    printf("Strings in lexographical order :\n\n");
+    printf("\n------------------------------------------\n\nStrings in lexographical order :\n\n");
     for(int i = 0 ; i < strCount ; i++)
-        printf("%s\n", str + i);
+        printf("%2d. %s\n", i + 1, str + i);
+    printf("\n\n\n");
     return 0;
 }
